e047ae6a0e674b6bbf95be504b52186092b0f672
anuarmenco <anuarmenco@gmail.com>
2024-10-25 20:56:31 +0000

    Add a function in main.py

--- main.py
+++ main.py
@@ -18,6 +18,10 @@ def bob():
     val = {"value": "bob"}
     return jsonify(val)
 
+@app.route('/papa')
+def papa():
+    val = {"value": "papa"}
+    return jsonify(val)
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
